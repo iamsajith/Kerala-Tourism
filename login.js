836e9a87c9b1d0e1382612be0e614f5366e5aed1
iamsajith <sajithj03@gmail.com>
2022-03-23 11:36:12 +0530

    added form validation

---
+++ login.js
@@ -0,0 +1,52 @@
+const form = document.getElementById('login');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+
+
+if(emailValue === '') {
+ setErrorFor(email, 'Email cannot be blank');
+} else if (!isEmail(emailValue)) {
+ setErrorFor(email, 'Not a valid email');
+} else {
+ setSuccessFor(email);
+}
+if(passwordValue === '') {
+ setErrorFor(password, 'Password cannot be blank');
+}
+ else if(!isPassword(passwordValue)){ 
+   setErrorFor(password, 'Not a valid password');
+ }
+ else{
+ setSuccessFor(password);
+}
+
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'control success';
+}
+
+function isEmail(email) {
+	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+function isPassword(password){
+ return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(password)
+}
---
+++ signup.js
@@ -0,0 +1,111 @@
+const form2 = document.getElementById('login2');
+const form = document.getElementById('login');
+const firstname = document.getElementById('firstName');
+const lastname = document.getElementById('lastName');
+const phone = document.getElementById('phone');
+const email_ = document.getElementById('emailAddress');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('confirmPassword');
+
+form.addEventListener('submit',(Event)=>{
+Event.preventDefault();
+checkInputsUp();
+});
+
+form.addEventListener('submit',(Event)=>{
+ Event.preventDefault();
+ checkInputsIn();
+ });
+function checkInputsUp(){
+ const firstnameVal = firstname.value.trim();
+ const lastnameVal = lastname.value.trim();
+ const phoneVal = phone.value.trim();
+ const emailVal= email_.value.trim();
+ const passwordVal = password.value.trim();
+ const cpasswordVal = cpassword.value.trim();
+ let regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+ let name = /^[A-Za-z\s]+$/;
+ let passkey = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+if(firstnameVal===''){
+ setErrorFor(firstname,'First name can\'t be blank');
+}
+else if(name.test(firstnameVal)){
+setSuccessFor(firstname);
+
+ }
+ else{
+  setErrorFor(firstname,'First name is not valid')
+ }
+
+
+ if(lastnameVal===''){
+  setErrorFor(lastname,'Last name can\'t be blank');
+ }
+ else if(name.test(lastnameVal)){
+ setSuccessFor(lastname);
+ 
+  }
+  else{
+   setErrorFor(lastname,'Last name is not valid')
+  }
+
+  if(phoneVal===''){
+    setErrorFor(phone,'Phone number can\'t be blank');
+   }
+   else if(phoneno.test(phoneVal)){
+   setSuccessFor(phone);
+  }
+    else{
+     setErrorFor(phone,'Phone number is not valid')
+    }
+
+  if(emailVal===''){
+    setErrorFor(email_,'E-mail can\'t be blank');
+   }
+   else if(regexp.test(emailVal)){
+   setSuccessFor(email_);
+   
+    }
+    else{
+     setErrorFor(email_,'E-mail is not valid');
+    }
+if(passwordVal===''){
+    setErrorFor(password,'Password can\'t be blank');
+}
+else if(passkey.test(passwordVal)){
+  setSuccessFor(password);
+ }
+  
+ else{
+  setErrorFor(password,'Password is not valid');
+ }
+
+if(cpasswordVal===''){
+    setErrorFor(cpassword,'Password can\'t be blank');
+}
+
+else if(passwordVal !== cpasswordVal) {
+  setErrorFor(cpasswordVal, 'Passwords does not match');
+ }
+else if(passkey.test(cpasswordVal)){
+  setSuccessFor(cpassword);
+ }
+
+
+ else{
+  setErrorFor(cpassword,'Password is not valid');
+ }
+}
+
+function setErrorFor(input,message){
+ const formControl = input.parentElement;
+ const small =formControl.querySelector('small');
+ small.innerHTML = message;
+ formControl.className = 'control error';
+}
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'control success';
+}
